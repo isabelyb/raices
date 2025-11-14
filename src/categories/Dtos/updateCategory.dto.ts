22acf87639d9268d7b6485575e2653ae556951bb
Tatiana Bayona <tatianabayona2017@gmail.com>
2025-11-14 15:04:02 -0500

    Se agregan los Dtos de Categories con sus archivos

---
+++ src/categories/Dtos/updateCategory.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty, PartialType } from "@nestjs/swagger";
+import { CreateCategoryDto } from "./createCategory.dto";
+import { IsNotEmpty, IsString, IsUUID } from "class-validator";
+
+export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {
+  @ApiProperty({
+    description: 'UUID de la categoria a actualizar',
+  })
+  @IsNotEmpty({ message: 'El id de la categoria es obligatorio' })
+  @IsString({ message: 'El id debe ser una cadena de caracteres' })
+  @IsUUID('4', { message: 'El id de la categoria debe tener un formato UUID' })
+  id: string;
+}
